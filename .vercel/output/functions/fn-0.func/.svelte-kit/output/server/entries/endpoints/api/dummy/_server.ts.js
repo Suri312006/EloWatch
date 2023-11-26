bfddd113ed9b1d2540629678df5cf0055d744e46
Surendra Jammishetti <suri312006@gmail.com>
2023-11-26 10:03:38 -0800

    axios dependency error lol

--- .vercel/output/functions/fn-0.func/.svelte-kit/output/server/entries/endpoints/api/dummy/_server.ts.js
+++
@@ -1,8 +0,0 @@
-import "../../../../chunks/index.js";
-const GET = async () => {
-  console.log("the towers have been hit");
-  return new Response(JSON.stringify({ "message": "hi" }), { status: 200 });
-};
-export {
-  GET
-};
