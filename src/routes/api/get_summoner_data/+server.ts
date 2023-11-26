d151167ed9e01631b9499803b0de77de79b63818
Surendra Jammishetti <suri312006@gmail.com>
2023-11-25 21:18:06 -0800

    vercel fix?

--- src/routes/api/get_summoner_data/+server.ts
+++ src/routes/api/get_summoner_data/+server.ts
@@ -1,5 +1,11 @@
 import { Client } from 'shieldbow';
 
+export const config = {
+	runtime: 'nodejs18.x'
+};
+
+
+
 export async function GET () {
     
 const client = new Client('RGAPI-a06fe8b7-5b34-43e0-8cc4-28c6ca64ccb4');
@@ -32,7 +38,7 @@ await client
   });
 
   let myJson:object = {Kayn_data: data}
-
+console.log("in_api", myJson)
   return new Response(JSON.stringify({Kayn_data: data}), {status: 200})
 
 }
