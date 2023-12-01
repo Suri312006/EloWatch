4ec8712a90d4eae7cdeddef1435a6b3a7e342899
Surendra Jammishetti <suri312006@gmail.com>
2023-11-30 16:57:42 -0800

    no more exposed api key!!

--- src/routes/api/get_summoner_data/+server.ts
+++ src/routes/api/get_summoner_data/+server.ts
@@ -1,49 +1,10 @@
 import { Client } from 'shieldbow';
 
 import { LolApi, Constants } from 'twisted'
+import { RIOT_API_KEY } from '$env/static/private'
 
-export const config = {
-	runtime: 'nodejs16.x'
-};
 
-// export async function GET () {
-    
-//   const client = new Client('RGAPI-a06fe8b7-5b34-43e0-8cc4-28c6ca64ccb4');
-  
-//   let data:String = ''
-  
-//   await client
-//     .initialize({
-//       region: 'na', // defaults to 'na' anyways.
-//     })
-//     .then(async () => {
-//       // After initialization, you can use the client to make requests
-//       // For example, lets fetch the following details of a summoner
-//       // - Summoner name, summoner level
-//       // - SoloQ ranking and LP
-//       // - The highest champion mastery
-//       // const summoner = await client.summoners.fetchBySummonerName('TheDrone7');
-//       // const leagueEntry = await summoner.fetchLeagueEntries();
-//       // const championMastery = summoner.championMastery;
-//       // const soloQ = leagueEntry.get('RANKED_SOLO_5x5');
-//       // const highest = await championMastery.highest();
-//       // console.log(`Summoner name: ${summoner.name} (level: ${summoner.level}).`);
-//       // console.log(`SoloQ: ${soloQ?.tier} ${soloQ?.division} (${soloQ?.lp} LP).`);
-//       // console.log(`Highest champion mastery: ${highest.champion.name} (M${highest.level} ${highest.points} points).`);
-//       const kayn = await client.champions.fetch('Kayn');
-  
-//       data = kayn.lore
-      
-  
-//     });
-  
-//     let myJson:object = {Kayn_data: data}
-//   console.log("in_api", myJson)
-//     return new Response(JSON.stringify({Kayn_data: data}), {status: 200})
-  
-//   }
-
-const api = new LolApi({key: "RGAPI-a06fe8b7-5b34-43e0-8cc4-28c6ca64ccb4"})
+const api = new LolApi({key: RIOT_API_KEY})
 
 
 export async function GET () {
