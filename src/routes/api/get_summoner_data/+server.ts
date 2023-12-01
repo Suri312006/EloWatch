593c1bd304da19bfa512c2998016c0367655fe99
Surendra Jammishetti <suri312006@gmail.com>
2023-12-01 11:10:03 -0800

    holy moly it finally works, fixed env stuff for vercel

--- src/routes/api/get_summoner_data/+server.ts
+++ src/routes/api/get_summoner_data/+server.ts
@@ -1,19 +1,29 @@
 import { Client } from 'shieldbow';
 
 import { LolApi, Constants } from 'twisted'
-import { RIOT_API_KEY} from '$env/static/private'
+import { MY_RIOT_API_KEY} from '$env/static/private'
 
 
-const api = new LolApi({key: RIOT_API_KEY})
+const api = new LolApi({key: MY_RIOT_API_KEY})
 
 
 export async function GET () {
-  console.log(RIOT_API_KEY)
+  console.log(MY_RIOT_API_KEY)
   let summoner = await api.Summoner.getByName('Hide on bush', Constants.Regions.KOREA)
   
-  // let summoner2 = await api.Summoner.getByName('SirYum', Constants.Regions.AMERICA_NORTH)
+   let summoner2 = await api.Summoner.getByName('SirYum', Constants.Regions.AMERICA_NORTH)
 
- // return new Response(JSON.stringify({Kayn_data: summoner2.response.id.toString()}), {status: 200})
-  return new Response(JSON.stringify({Kayn_data: "hi"}), {status: 200})
+ return new Response(JSON.stringify({Kayn_data: summoner2.response.id.toString()}), {status: 200})
+
 }
+
+
+//* code that should work
+// const api = new LolApi({key: MY_RIOT_API_KEY})
+
+// export async function GET () {
+
+//   let summoner = await api.Summoner.getByName('Hide on bush', Constants.Regions.KOREA)
+//   return new Response(JSON.stringify({Kayn_data: summoner.response.id.toString()}), {status: 200})
+// }
 
