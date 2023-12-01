0ed6f8ab910607deeac77dca23156b9ce6b2d75b
Surendra Jammishetti <suri312006@gmail.com>
2023-11-30 17:10:42 -0800

    no more exposed key lol!

--- src/routes/api/get_summoner_data/+server.ts
+++ src/routes/api/get_summoner_data/+server.ts
@@ -1,15 +1,17 @@
 import { Client } from 'shieldbow';
 
 import { LolApi, Constants } from 'twisted'
-import { RIOT_API_KEY } from '$env/static/private'
+import { RIOT_DEV_API_KEY} from '$env/static/private'
 
 
-const api = new LolApi({key: RIOT_API_KEY})
+const api = new LolApi({key: RIOT_DEV_API_KEY})
 
 
 export async function GET () {
+  console.log(RIOT_DEV_API_KEY)
 
   let summoner = await api.Summoner.getByName('Hide on bush', Constants.Regions.KOREA)
-  return new Response(JSON.stringify({Kayn_data: summoner.response.id.toString()}), {status: 200})
+  let summoner2 = await api.Summoner.getByName('SirYum', Constants.Regions.AMERICA_NORTH)
+  return new Response(JSON.stringify({Kayn_data: summoner2.response.id.toString()}), {status: 200})
 }
 
