0228c477dae1af5c974ae2a787c5744db0de5d8c
Surendra Jammishetti <suri312006@gmail.com>
2023-12-02 01:18:45 -0800

    working!

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -6,6 +6,11 @@
 	import { draw } from 'svelte/transition';
 	import { popup } from '@skeletonlabs/skeleton';
 	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	import { goto } from '$app/navigation';
+
+	import { page } from '$app/stores';
+
+
 
 	const drawerStore = getDrawerStore();
 
@@ -16,18 +21,34 @@
 	};
 
 	const drawerSettings: DrawerSettings = {
-	id: 'nav-drawer',
-	// Provide your property overrides:
-	bgDrawer: 'bg-secondary-900 text-white',
-	bgBackdrop: 'bg-gradient-to-tr from-primary-500/50 via-trinary-500/50 to-secondary-500/50',
+		id: 'nav-drawer',
+		// Provide your property overrides:
+		bgDrawer: 'bg-secondary-900 text-white',
+		bgBackdrop: 'bg-gradient-to-tr from-primary-500/50 via-trinary-500/50 to-secondary-500/50'
+	};
 
-};
+	let formData = {
+		SummonerName: ''
+	};
 
-const fancyDrawerStore = () => {
-	drawerStore.open(drawerSettings)
-}
+	const SummonerLink = (event: Event) => {
+		event.preventDefault();
 
+		goto(`/lol/${formData.SummonerName}`);
 
+		formData.SummonerName = '';
+	};
+
+	const fancyDrawerStore = () => {
+		drawerStore.open(drawerSettings);
+	};
+
+    //* yo reactive component!!!
+    $: show_search = $page.url.pathname != "/"
+
+    
+
+    console.log(show_search)
 </script>
 
 <AppBar
@@ -37,25 +58,36 @@ const fancyDrawerStore = () => {
 	slotTrail="place-content-end"
 >
 	<svelte:fragment slot="lead">
-		<a class="flex justify-evenly items-center" href="/">
+		<a class="flex items-center justify-evenly" href="/">
 			<!-- temporary logo-->
-			<img src="cropped-logo.png" alt="logo" class=" h-[5rem] p-3 mt-1" />
+
+			<!-- <img src="cropped-logo.png" alt="logo" class=" h-[5rem] p-3 mt-1" /> -->
+
 			<!-- can add name here after, shouldnt scale from desktop to mobile-->
-			<h1 class="font-bold text-5xl pt-2">TRACE</h1>
+			<h1 class="pt-2 text-5xl font-bold hover:text-primary-400">EloWatch</h1>
 		</a>
 	</svelte:fragment>
 
 	<!-- end section -->
 	<svelte:fragment slot="trail">
-		<div id="desktop-View" class="max-md:hidden justify-between mr-10">
-			<ul class="flex justify-evenly text-3xl items-center font-medium space-x-5 min-w-[25rem]">
-				<li><a href="/events" class="hover:text-primary-400 font-bold">EVENTS</a></li>
-				<li>|</li>
-				<li><a href="/resources" class="hover:text-primary-400 font-bold">RESOURCES</a></li>
-				<li>|</li>
-				<li><a href="/about_us" class="hover:text-primary-400 font-bold">ABOUT</a></li>
-				<li>|</li>
-				<button  use:popup={popupClick} class="bg-transparent hover:text-primary-400 font-bold"> SOCIALS </button>
+	{#if show_search}
+    <form method="POST" on:submit={SummonerLink}>
+        <input
+            type="text"
+            id="summoner_name"
+            bind:value={formData.SummonerName}
+            autocomplete="off"
+            placeholder="Summoner name..."
+        />
+    </form>
+
+    {/if}	
+
+		<div id="desktop-View" class="mr-10 justify-between max-md:hidden">
+			<ul class="flex min-w-[25rem] items-center justify-evenly space-x-5 text-3xl font-medium">
+				<button use:popup={popupClick} class="bg-transparent font-bold hover:text-primary-400">
+					SOCIALS
+				</button>
 				<!-- <li class='flex items-center'>
 				  <a href="/contact">Contact</a>
 				  <CaretRight class="h-[1.5rem] w-[1.5rem]"/>
@@ -65,24 +97,28 @@ const fancyDrawerStore = () => {
 
 		<!--popup for socials-->
 		<div class="card variant-filled-primary mt-1" data-popup="popupClick">
-			<ul class="flex flex-col justify-center items-center space-y-2">
+			<ul class="flex flex-col items-center justify-center space-y-2">
 				<li>
-					<a href="https://instagram.com/trace.ucsc?igshid=MmVlMjlkMTBhMg=="  target="_blank" class="btn">
-						<i class="fab fa-instagram hover:text-primary-400 text-2xl"></i>
+					<a
+						href="https://instagram.com/trace.ucsc?igshid=MmVlMjlkMTBhMg=="
+						target="_blank"
+						class="btn"
+					>
+						<i class="fab fa-instagram text-2xl hover:text-primary-400" />
 					</a>
 				</li>
 				<li>
 					<a href="https://github.com/T-R-A-C-E" target="_blank" class="btn">
-						<i class="fab fa-github hover:text-primary-400 text-2xl"></i>
+						<i class="fab fa-github text-2xl hover:text-primary-400" />
 					</a>
 				</li>
 				<li>
 					<a href="https://discord.gg/wUWu7hJPds" target="_blank" class="btn">
-						<i class="fab fa-discord hover:text-primary-400 text-2xl"></i>
+						<i class="fab fa-discord text-2xl hover:text-primary-400" />
 					</a>
 				</li>
 			</ul>
-			<div class="arrow bg-surface-100-800-token"></div>
+			<div class="bg-surface-100-800-token arrow" />
 		</div>
 
 		<!-- mobile view -->
