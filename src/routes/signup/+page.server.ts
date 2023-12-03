c6a3e2756deb4c8b7edae9342347c525503ea18d
Surendra Jammishetti <suri312006@gmail.com>
2023-12-02 23:55:50 -0800

    holy moly signup works!

--- src/routes/signup/+page.server.ts
+++ src/routes/signup/+page.server.ts
@@ -1,11 +1,38 @@
+import { fail } from '@sveltejs/kit'
+
 export const actions = {
     signup: async(event) => {
 
-        let lol = await event.request.formData()
-        console.log( lol )
+        let formdata = await event.request.formData()
 
+        let email = formdata.get('email')
+        let password = formdata.get("password")
+        let password_check = formdata.get("password_check")
         //* need a simple password check here, and then send off data to supabase
+        if (password != password_check){
+            return fail(400, {email, password_mismatch: true})
+        }
+        
+        let { data, error } = await event.locals.supabase.auth.signUp({
+            email: email!.toString(),
+            password: password!.toString()
+          })
+        
+          console.log(data)
 
-        // event.locals.supabase.auth.signUp()
+        //best way to handle this??
+        if (data.user?.aud == 'authenticated'){
+            console.log()
+            return {user: data.user} 
+        }
+        else {
+            return false
+        }
+
+        
+
+
+
+  
     }
 }
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,21 +1,25 @@
-
 <script lang="ts">
+	import type { ActionData } from './$types';
 
+    export let data;
 
+	export let form: ActionData;
 
-
+    console.log(data.session)
 </script>
-
 
 <h1>looks like you dont have an account!</h1>
 
-<h1> lets get you signed up bud!</h1>
+<h1>lets get you signed up bud!</h1>
 
-<form method="POST" action='?/signup'>
+<form method="POST" action="?/signup">
+	<input name="email" type="email" placeholder="Email" value={form?.email ?? ''}/>
+	<input name="password" type="password" placeholder="password"  />
+	<input name="password_check" type="password" placeholder="repeat password" />
 
-    <input name="email" type="email" placeholder="Email">
-    <input name="password" type="password" placeholder="password">
-    <input name="password_check" type="password" placeholder="repeat password">
+	<button> signup</button>
 
-    <button> signup</button>
+	{#if form?.password_mismatch}
+		<p class=" text-2xl">passwords dont match monkey</p>
+	{/if}
 </form>
